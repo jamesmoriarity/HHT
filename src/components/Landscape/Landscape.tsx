17cb51398b22a6fc8aa93d0a12e88b2a6adfc2ff
jamesmoriarity <james@moriarity.org>
2022-02-18 19:07:43 -1000

    code cleanup

--- src/components/Landscape/Landscape.tsx
+++ src/components/Landscape/Landscape.tsx
@@ -4,57 +4,47 @@ import { gsap } from 'gsap'
 import './Landscape.css'
 
 export default function Landscape(props:any){
+    const ref = useRef(null)
     const location = useLocation();
     const [pathName, setPathName] = useState(location.pathname)
     const [hasMoved, setHasMoved] = useState(false)
-    let ref = useRef(null)
-    useEffect(() => {
-        console.log('ua-land')
+    const handleLocationChange = function(){
         if(pathName == location.pathname && hasMoved){
-            console.log('same location');
             return
         }
         setPathName(location.pathname)
-        if(location.pathname === '/hawaii'){
-            moveTo('hawaii')
-            return
+        switch(location.pathname){
+            case '/hawaii':
+                moveTo('hawaii')
+                break
+            case '/washington':
+                moveTo('washington')
+                break
+            default:
+                moveTo('home')
         }
-        if(location.pathname === '/washington'){
-            moveTo('washington')
-            return
-        }
-        moveTo('home')
-    }, [location]); 
+    } 
     const getLocationName = function(pathName:string){
         let name:string = pathName.substring(1)
         return (name === '') ? 'home' : name
     }
+    const getX = function(locationName:string){
+        let locations:Map<string, number> = new Map()
+        locations.set('home', 400)
+        locations.set('hawaii', 10)
+        locations.set('washington', 800)
+        let x:number | undefined = locations.get(locationName)
+        if(x === undefined){
+            return 400
+        }
+        return x
+    }
     const moveTo = function(locationName:string){
-        console.log('moveTo locationName', locationName)
-        let x:number = 0
-        switch(locationName){
-            case 'home':
-                x = 400;
-                break
-            case 'hawaii':
-                x = 10;
-                break
-            case 'washington':
-                x = 800;
-                break
-            default:
-                x = 0
-        }
-        if(!hasMoved){
-            gsap.set(ref.current, {x:x})
-        }
-        else{
-            gsap.to(ref.current, {x:x})
-        }
+        const x:number = getX(locationName)
+        const duration:number = (hasMoved) ? 1 : 0
+        gsap.to(ref.current, {x:x, duration:duration})
         setHasMoved(true)
     }
-    /*
-        build a scene 
-    */
+    useEffect(handleLocationChange, [location]);
     return <div id="landscape" ref={ref}>Lanscape Component: {getLocationName(pathName)}</div>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,5 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import Website from './components/Website/Website';
 
-/* ReactDOM.render(
-  <React.StrictMode>
-      <App/>
-  </React.StrictMode>,
-  document.getElementById('root')
-); */
-
-ReactDOM.render(<Website/>,
-  document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Website/>, document.getElementById("root"));
 serviceWorker.unregister();
