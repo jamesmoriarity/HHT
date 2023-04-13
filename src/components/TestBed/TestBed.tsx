abac0d6b3048295e254b155d3da1fb86fc9cb7b8
jamesmoriarity <james@moriarity.org>
2023-04-13 10:14:14 -1000

    bird working

--- src/components/TestBed/TestBed.tsx
+++ src/components/TestBed/TestBed.tsx
@@ -5,7 +5,17 @@ import * as THREE from "three";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 import { GUI, GUIController } from 'dat.gui'
 import { ImageCodes } from "./ImageCodes";
-
+export interface ValueRange{
+    min:number
+    max:number
+    step:number
+}
+export interface ControlRanges{
+    x:ValueRange
+    y:ValueRange
+    z:ValueRange
+    scale:ValueRange
+}
 export default function TestBed(){
     const buildScene = () => {
         const scene = new THREE.Scene();
@@ -23,7 +33,7 @@ export default function TestBed(){
             .name('Camera FOV')
             .listen()
             .onChange(onCameraChange)
-        cameraFolder.add(camera.rotation, 'y', Math.PI / -1, Math.PI)
+        cameraFolder.add(camera.rotation, 'y', Math.PI / -1, Math.PI, .01)
                 .name('Rotate on Y axis')
                 .listen()
                 .onChange(onCameraChange)
@@ -107,62 +117,115 @@ export default function TestBed(){
         buildSeattleControls(seattleMesh, seattleMeshProps, onObjectChange)
         return seattleMesh
     }
-    const buildForest = (hostScene:THREE.Scene) => {
-        const textureLoader = new THREE.TextureLoader();
-        const forestTexture = textureLoader.load(ImageCodes.forest);
-        const forestMaterial = new THREE.MeshPhongMaterial({map:forestTexture, transparent:true})
-        let forestPlane:THREE.PlaneGeometry = new THREE.PlaneGeometry(8,2)
-        let forestMesh = new THREE.Mesh( forestPlane, forestMaterial);
-        let forestMeshProps = {scale:0.4}
-        forestMesh.position.set(-4, 0 , 0)
-        forestMesh.scale.x = forestMeshProps.scale
-        forestMesh.scale.y = forestMeshProps.scale
-        forestMesh.scale.z = forestMeshProps.scale
-        hostScene.add(forestMesh)
-        const forestFolder = gui.addFolder('forest')
-        forestFolder.add(forestMesh.position, 'x', -10, 10,1)
+    const buildSeattleSprite = (hostScene:THREE.Scene):THREE.Sprite => {
+        const position:THREE.Vector3 = new THREE.Vector3(-3,-3,-0.2)
+        const imageCode:string = ImageCodes.seattleSkyline
+        const aspectRatio:number = 4
+        const initialScale:number = 0.4
+        const sprite:THREE.Sprite = buildSprite(hostScene, position, imageCode, aspectRatio, initialScale)
+        buildSpriteControls('forest sprite', sprite, aspectRatio, initialScale)
+        return sprite
+    }
+    const buildForestSprite = (hostScene:THREE.Scene):THREE.Sprite => {
+        const position:THREE.Vector3 = new THREE.Vector3(-6,-0.1,-2.3)
+        const imageCode:string = ImageCodes.forest
+        const aspectRatio:number = 4
+        const initialScale:number = 1
+        const sprite:THREE.Sprite = buildSprite(hostScene, position, imageCode, aspectRatio, initialScale)
+        buildSpriteControls('forest sprite', sprite, aspectRatio, initialScale)
+        return sprite
+    }
+    const buildBird = (hostScene:THREE.Scene):THREE.Sprite => {
+        const position:THREE.Vector3 = new THREE.Vector3(-4,8,0)
+        const imageCode:string = ImageCodes.bird
+        const aspectRatio:number = 1
+        const initialScale:number = 1
+        const sprite:THREE.Sprite = buildSprite(hostScene, position, imageCode, aspectRatio, initialScale)
+        buildSpriteControls('bird', sprite, aspectRatio, initialScale)
+        return sprite
+    }
+    
+    const animateBird = () => {
+        bird.position.x = bird.position.x + .2
+        bird.position.y = bird.position.y - .015
+        bird.position.z = bird.position.z - .3
+        if (bird.position.x < 65) setTimeout(animateBird, 40)
+        else{
+            const orginalBirdPosition = new THREE.Vector3(-4,8,0)
+            bird.position.set(orginalBirdPosition.x, orginalBirdPosition.y, orginalBirdPosition.z)
+        }
+        onObjectChange()  
+    }
+    const buildSpriteControls = (   folderName:string, 
+                                    sprite:THREE.Sprite, 
+                                    aspectRatio:number, 
+                                    initialScale:number,
+                                    ranges?:ControlRanges) => {
+
+        if(!ranges) ranges =   {
+                                x:{min:-10, max:10, step:0.1},
+                                y:{min:-10, max:10, step:0.1},
+                                z:{min:-10, max:10, step:0.1},
+                                scale:{min:0.1, max:10, step:0.1}
+                                }
+
+        let meshProps = {scale:initialScale}
+        const folder = gui.addFolder(folderName)
+        folder.add(sprite.position, 'x', ranges.x.min, ranges.x.max, ranges.x.step)
             .name('Move X')
             .listen()
             .onChange(onObjectChange)
-        forestFolder.add(forestMesh.position, 'z', -10, 10,1)
-            .name('Move Z')
-            .listen()
-            .onChange(onObjectChange)
-        forestFolder.add(forestMesh.position, 'y', -2, 2,0.1)
+         
+            folder.add(sprite.position, 'y', ranges.y.min, ranges.y.max, ranges.y.step)
             .name('Move Y')
             .listen()
             .onChange(onObjectChange)
-            forestFolder.add(forestMeshProps, 'scale', 0.2, 3, 0.1)
+            folder.add(sprite.position, 'z', ranges.z.min, ranges.z.max, ranges.z.step)
+            .name('Move Z')
+            .listen()
+            .onChange(onObjectChange)
+            folder.add(meshProps, 'scale', ranges.scale.min, ranges.scale.max, ranges.scale.step)
             .name('Scale')
             .listen()
             .onChange((scaleValue)=>{
-                forestMesh.scale.x = scaleValue
-                forestMesh.scale.y = scaleValue
-                forestMesh.scale.z = scaleValue
+                sprite.scale.x = scaleValue * aspectRatio
+                sprite.scale.y = scaleValue
+                sprite.scale.z = scaleValue
                 onObjectChange()
             })
-        forestFolder.open()
-
-
-        return forestMesh
+        
+        folder.open()
     }
+    const buildSprite = (   hostScene:THREE.Scene, 
+                            position:THREE.Vector3, 
+                            imageCode:string, 
+                            aspectRatio:number, 
+                            initialScale:number) => {
+            // aspectRatio defines w/h ratio. w = h * aspectRatio
+            const textureLoader = new THREE.TextureLoader();
+            const texture = textureLoader.load(imageCode);
+            const material = new THREE.SpriteMaterial( { map: texture } );
+            const sprite = new THREE.Sprite( material );
+            const xScale:number = initialScale * aspectRatio
+            const yScale:number = initialScale 
+            const zScale:number = 1
+            sprite.scale.set(xScale, yScale, zScale)
+            sprite.position.set(position.x, position.y, position.z)
+            hostScene.add( sprite );
+            return sprite
+    }
+
     const buildForestTwo = (hostScene:THREE.Scene) => {
-
-
         const textureLoader = new THREE.TextureLoader();
         const forestTexture = textureLoader.load(ImageCodes.closeForest);
-        const forestMaterial = new THREE.MeshPhongMaterial({map:forestTexture, transparent:true})
-
-        const ratio:number = 2
-
-        const material = new THREE.SpriteMaterial( { map: forestTexture } );
-
-        const sprite = new THREE.Sprite( material );
+        const aspectRatio:number = 2
+        const forestMaterial = new THREE.SpriteMaterial( { map: forestTexture } );
+        const sprite = new THREE.Sprite( forestMaterial );
         const xScale:number = 3
-        const yScale:number = xScale/ratio
+        const yScale:number = xScale/aspectRatio
         const zScale:number = 1
         sprite.scale.set(xScale, yScale, zScale)
-        sprite.position.set(-6, 0 , 0.2)
+        sprite.position.set(-6, 0.3 , 0.2)
         hostScene.add( sprite );
         let forestMeshProps = {scale:1}
         const forestTwoFolder = gui.addFolder('forest two')
@@ -184,7 +247,7 @@ export default function TestBed(){
             .listen()
             .onChange((scaleValue)=>{
                 sprite.scale.x = scaleValue
-                sprite.scale.y = scaleValue/ratio
+                sprite.scale.y = scaleValue/aspectRatio
                 sprite.scale.z = scaleValue
                 onObjectChange()
             })
@@ -194,8 +257,6 @@ export default function TestBed(){
 
         return sprite
     }
-
-
 
     const buildSeattleControls = (seattleMesh:THREE.Mesh, seattleMeshProps:any, onChangeHandler:Function) => {
         const seattleFolder = gui.addFolder('seattle')
@@ -223,6 +284,9 @@ export default function TestBed(){
         seattleFolder.open()
     }
 
+    const animate = () => {
+        animateBird()
+    }
     const gui = new GUI()
     const scene:THREE.Scene = buildScene()
     const camera:THREE.PerspectiveCamera = buildCamera()
@@ -230,17 +294,19 @@ export default function TestBed(){
     const [ambientLight, directionalLight] = buildLighting() 
     const mountain:THREE.Mesh = buildMountain(scene)
     const seattle:THREE.Mesh = buildSeattle(scene)
-    const forest:THREE.Mesh = buildForest(scene)
+    //const forest:THREE.Mesh = buildForest(scene)
     const forestTwo:THREE.Sprite = buildForestTwo(scene)
+    const bird:THREE.Sprite = buildBird(scene)
+    const fsprite:THREE.Sprite = buildForestSprite(scene)
 
     useEffect(()=>{
             console.log('useeffect')
             document.getElementById('testbed-inner')!.appendChild( renderer.domElement );
-            camera.lookAt(0,0,0)
             renderScene()  
+            
         } 
     )
     return  <div id="testbed-container">
-                <div id="testbed-inner"/>
+                <div onClick={animate} id="testbed-inner"/>
             </div>
 }
